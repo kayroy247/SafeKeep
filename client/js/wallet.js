a3b4d2322d5ea0ce9f50853fc0478e7c7e96a552
kayroy247 <okunladekayode@gmail.com>
2020-11-18 00:14:08 +0100

    bg:fix get balance

--- client/js/wallet.js
+++ client/js/wallet.js
@@ -1,91 +1,118 @@
 // SafeKeepABI is available here because SafeKeep.js is imported in wallet.html
-const usersBalance = document.querySelector('.user-balance');
-const depositForm = document.querySelector('.deposit-form');
-const withdrawForm = document.querySelector('.withdraw-form');
-const depositAmount = document.querySelector('.deposit-amount');
-const depositButton = document.querySelector('.deposit-button');
-const backupAddress = document.querySelector('.backup-address');
-const withdrawAmount = document.querySelector('.withdraw-amount');
-const pingButton = document.querySelector('.ping-button');
+let usersBalance,
+  depositForm,
+  withdrawForm,
+  depositAmount,
+  depositButton,
+  backupAddress,
+  withdrawAmount,
+  pingButton,
+  hashRegex,
+  contract,
+  web3;
+function getDomNodes() {
+  usersBalance = document.querySelector('.user-balance');
+  depositForm = document.querySelector('.deposit-form');
+  withdrawForm = document.querySelector('.withdraw-form');
+  depositAmount = document.querySelector('.deposit-amount');
+  depositButton = document.querySelector('.deposit-button');
+  backupAddress = document.querySelector('.backup-address');
+  withdrawAmount = document.querySelector('.withdraw-amount');
+  pingButton = document.querySelector('.ping-button');
 
-const contractAddress = '0xa055dFC2190bA3C147D96C69eD5e11442A59525f';
-const hashRegex = /^0x([A-Fa-f0-9]{64})$/;
-let contract;
-let web3;
+  contractAddress = '0xa055dFC2190bA3C147D96C69eD5e11442A59525f';
+  hashRegex = /^0x([A-Fa-f0-9]{64})$/;
 
-if (window.ethereum) {
-  web3 = new Web3(window.ethereum);
+  if (window.ethereum) {
+    web3 = new Web3(window.ethereum);
+  }
+  // Get the contract instance.
+  contract = new web3.eth.Contract(SafeKeepABI[0].abi, contractAddress);
 }
-// Get the contract instance.
-contract = new web3.eth.Contract(
-  SafeKeepABI[0].abi,
-  contractAddress
-);
-
 window.addEventListener('load', async () => {
-  const userBalance = await contract.methods.getBalance().call();
-  usersBalance.innerText = `${userBalance} ETH`;
-})
-
+  getDomNodes();
+  depositEvent();
+  withdrawEvent();
+  try {
+    const acct = await web3.eth.getAccounts();
+    const userBalance = await contract.methods
+      .getBalance()
+      .call({from: acct[0]});
+    console.log(userBalance);
+  } catch (error) {
+    console.log(error);
+  }
+  usersBalance.innerText = `${web3.utils.fromWei(userBalance, 'ether')} ETH`;
+});
 
 const Notificate = (msg, status, timeout = 2500) => {
   UIkit.notification(msg, {
     status,
     pos: 'top-center',
-    timeout
-  })
+    timeout,
+  });
+};
+
+function depositEvent() {
+  return depositForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const accounts = await web3.eth.getAccounts();
+    const depo = depositAmount.value;
+    const backup = backupAddress.value;
+
+    if (web3.utils.isAddress(backup) && Number(depo) > 0.001) {
+      try {
+        const amountToSend = web3.utils.toWei(depo, 'ether');
+        const trx = await contract.methods
+          .deposit(backup)
+          .send({from: accounts[0], value: amountToSend});
+
+        if (hashRegex.test(trx.tx)) {
+          const msg = `Transaction was successful
+          https://ropsten.etherscan.io/tx/${trx.tx}
+          `;
+          Notificate(msg, 'success', 6000);
+          depositAmount = 0;
+          backupAddress = '';
+        }
+      } catch (error) {
+        Notificate('Something went wrong', 'danger');
+        console.error(error.message);
+      }
+    } else {
+      const errMsg =
+        'Check that backup address is valid and deposit is greater than 0.001';
+      Notificate(errMsg, 'warning');
+      console.error(errMsg);
+    }
+  });
 }
 
-depositForm.addEventListener("submit", async (event) => {
-  event.preventDefault();
+function withdrawEvent() {
+  return withdrawForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
 
-  const accounts = await web3.eth.getAccounts();
-  const depo = depositAmount.value;
-  const backup = backupAddress.value
+    const accounts = await web3.eth.getAccounts();
+    const withDraw = withdrawAmount.value;
 
-
-  if (web3.utils.isAddress(backup) && Number(depo) > 0.001) {
     try {
-      const amountToSend = web3.utils.toWei(depo, "ether");
-      const trx = await contract.methods.deposit(backup).send({ from: accounts[0], value: amountToSend });
-    
+      const amountToWithdraw = web3.utils.toWei(
+        withDraw === '' ? '0' : withDraw,
+        'ether'
+      );
+      const trx = await contract.methods
+        .withdraw(amountToWithdraw)
+        .send({from: accounts[0]});
       if (hashRegex.test(trx.tx)) {
         const msg = `Transaction was successful
-        https://ropsten.etherscan.io/tx/${trx.tx}
-        `
-        Notificate(msg, 'success', 6000)
-        depositAmount = 0;
-        backupAddress = '';
+          https://ropsten.etherscan.io/tx/${trx.tx}
+          `;
+        Notificate(msg, 'success', 6000);
       }
     } catch (error) {
-      Notificate('Something went wrong', 'danger')
+      Notificate('Something went wrong', 'danger');
       console.error(error.message);
     }
-
-  } else {
-    const errMsg = 'Check that backup address is valid and deposit is greater than 0.001';
-    Notificate(errMsg, 'warning')
-    console.error(errMsg)
-  }
-});
-
-withdrawForm.addEventListener("submit", async (event) => {
-  event.preventDefault();
-
-  const accounts = await web3.eth.getAccounts();
-  const withDraw = withdrawAmount.value;
-
-  try {
-    const amountToWithdraw = web3.utils.toWei(withDraw === '' ? '0' : withDraw, "ether");
-    const trx = await contract.methods.withdraw(amountToWithdraw).send({ from: accounts[0] });
-    if (hashRegex.test(trx.tx)) {
-      const msg = `Transaction was successful
-        https://ropsten.etherscan.io/tx/${trx.tx}
-        `
-      Notificate(msg, 'success', 6000)
-    }
-  } catch (error) {
-    Notificate('Something went wrong', 'danger')
-    console.error(error.message);
-  }
-});
+  });
+}
--- client/js/web3-auto.js
+++ client/js/web3-auto.js
@@ -1,34 +1,39 @@
-const ethereumButton = document.querySelector('.enableEthereumButton');
+document.addEventListener('click', function () {
+  connectApp();
+});
 
+function connectApp() {
+  const ethereumButton = document.querySelector('.enableEthereumButton');
 
-ethereumButton.addEventListener('click', async () => {
+  ethereumButton.addEventListener('click', async () => {
     //Will Start the metamask extension
 
     if (window.ethereum) {
-        const web3 = new Web3(window.ethereum);
-        try {
-            // Request account access if needed
-            const accounts = await ethereum.request({
-                method: 'eth_requestAccounts'
-            });
-            
-          
-            if (accounts[0].length === 42) {
-                window.location.href = "wallet.html";
-            }
-           return web3;
-        } catch (error) {
-            console.error(error);
+      const web3 = new Web3(window.ethereum);
+      try {
+        // Request account access if needed
+        const accounts = await ethereum.request({
+          method: 'eth_requestAccounts',
+        });
+
+        if (accounts[0].length === 42) {
+          window.location.href = 'wallet.html';
         }
-        // Legacy dapp browsers...
-    } else if (window.web3) {
-        // Use Mist/MetaMask's provider.
-       const web3 = window.web3;
-       console.log('Injected web3 detected.');
-       return web3;
-      
-    } else {
-        const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
         return web3;
+      } catch (error) {
+        console.error(error);
+      }
+      // Legacy dapp browsers...
+    } else if (window.web3) {
+      // Use Mist/MetaMask's provider.
+      const web3 = window.web3;
+      console.log('Injected web3 detected.');
+      return web3;
+    } else {
+      const web3 = new Web3(
+        new Web3.providers.HttpProvider('http://localhost:8545')
+      );
+      return web3;
     }
-});
+  });
+}
