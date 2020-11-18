b26f37edda1e031593afa0513714fdc8f8983cd0
Adebayo <daramola.adebayo@yahoo.com>
2020-11-18 12:40:08 +0100

    .

--- client/js/web3-auto.js
+++ client/js/web3-auto.js
@@ -1,9 +1,10 @@
 const ethereumButton = document.querySelector('.enableEthereumButton');
+
+// 3 represents the ID of the ropsten network
+const network = 3;
 let web3;
 
 const checkNetwork = async () => {
-    // 3 represents the ID of the ropsten network
-    const network = 3;
     const networkId = await web3.eth.net.getId();
 
     if (networkId !== network) {
@@ -16,16 +17,13 @@ const checkNetwork = async () => {
 window.addEventListener('load', () => {
     if (window.ethereum) {
         web3 = new Web3(window.ethereum);
-        // return web3;
     } else if (window.web3) {
         // Use Mist/MetaMask's provider.
         web3 = window.web3;
         console.log('Injected web3 detected.');
-        // return web3;
 
     } else {
         web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
-        // return web3;
     }
 
     checkNetwork();
@@ -41,7 +39,6 @@ const Notificate = (msg, status, timeout = 3000) => {
 
 ethereumButton.addEventListener('click', async () => {
     //Will Start the metamask extension
-    const network = 3;
     const res = await checkNetwork();
 
     if (res !== network) {
